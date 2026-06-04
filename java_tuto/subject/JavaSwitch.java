1efbc0b930bd323f8602f7d0b60ed36cd86745a0
이경준 <gyeongjun@igyeongjun-ui-MacBookAir.local>
2026-06-04 18:15:57 +0900

    add switch, whileLoop

--- java_tuto/subject/JavaSwitch.java
+++ java_tuto/subject/JavaSwitch.java
@@ -3,8 +3,41 @@ package subject;
 public class JavaSwitch {
     public static void main(String[] agrs) {
 
-        // switch~case문: 조건 검사에 사용될 값을 기준으로 각 조건에 맞는 case를 '코드의 시작점'으로 정한다.
+        int count = 3;
 
+        // switch~case문: 조건 검사에 사용될 값을 기준으로 각 조건에 맞는 case를 '코드의 시작점'으로 정한다.
+        // 즉, 따로 'break;'를 추가하지 않는다면 시작점이 되는 곳에서 그 다음에 해당하는 모든 코드를 실행한다.
+        switch (count) {
+            case 1: // 조건 불충족
+                System.out.println("1");
+            case 2: // 조건 불충족
+                System.out.println("2");
+            case 3: // 조건 만족, 코드 시작, break; 없음 다음 case로 이동
+                System.out.println("3");
+            case 4: // 코드 실행, break; 없음 다음 case로 이동
+                System.out.println("4");
+            case 5: // 코드 실행, break; 없음 다음 case로 이동
+                System.out.println("5");
+            default: // 코드 실행 - default: 조건 값이 모든 case에 만족하지 못할 때 실행
+                System.out.println("default");
+        }
+
+        // break
+        // 코드 실행 중 break; 를 만나면 그 즉시 해당 코드블록을 탈출한다.(if문, switch~case문, for, while반복문 모두 포함)
+        switch (count) {
+            case 1: // 조건 불충족
+                System.out.println("1");
+            case 2: // 조건 불충족
+                System.out.println("2");
+            case 3: // 조건 만족, 코드 시작, break; 없음 다음 case로 이동
+                System.out.println("3");
+            case 4: // 코드 실행, break; 없음 다음 case로 이동
+                System.out.println("4"); break; // "4"출력 이후 break; -> switch문 탈출, 다음 case로 이동x
+            case 5:
+                System.out.println("5");
+            default:
+                System.out.println("default");
+        }
 
     }
 }
--- java_tuto/subject/JavaWhileLoop.java
+++ java_tuto/subject/JavaWhileLoop.java
@@ -3,5 +3,24 @@ package subject;
 public class JavaWhileLoop {
     public static void main(String[] agrs) {
 
+        int count = 5;
+
+        // 반복문: 말 그대로다, 코드 블록에 있는 코드를 조건에 따라 반복으로 실행한다.
+        // while문: 조건이 만족하면 코드 블록을 실행 -> 조건이 만족하고 조건에 변화가 없다면 이론상 영원히 실행하게된다.
+        while (count < 10) { // 조건이 만족한다면 코드 블록의 코드를 실행한다.
+            System.out.println(count);
+            count++; // count + 1; -> 조건 값에 변화를 주지 않으면 영원히 코드 블록을 실행하게 된다.
+        }
+
+        System.out.println();
+        count = 5; // count 값 초기화
+
+        // do~while문: do의 코드 블록을 일단 실행 한 뒤 다시 반복해서 실행할 지 이후 while문의 조건으로 판단
+        // while문과의 차이: while문은 조건에 따라 코드 블록을 아예 실행하지 않을 수 도 있지만 do~while문의 경우 일단 코드 블록을 최소 '1번'은 실행한다.
+        do { // 코드 블록을 실행
+            System.out.println(count);
+            count++;
+        } while(count < 10); // 조건이 만족한다면 다시 코드 블록을 실행
+
     }
 }
